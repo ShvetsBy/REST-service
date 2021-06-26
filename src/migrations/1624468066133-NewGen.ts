ebef90420d92721c7bdb565225ac71cebe21db50
Ivan Shvets <shvets.ivan@gmail.com>
2021-06-26 10:43:53 +0300

    add: project structure - files, env, packages

--- src/migrations/1624468066133-NewGen.ts
+++ src/migrations/1624468066133-NewGen.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NewGen1624468066133 implements MigrationInterface {
     name = 'NewGen1624468066133'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "<Task>" ALTER COLUMN "boardId" SET NOT NULL`);
+      await queryRunner.query('ALTER TABLE "<Task>" ALTER COLUMN "boardId" SET NOT NULL');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "<Task>" ALTER COLUMN "boardId" DROP NOT NULL`);
+      await queryRunner.query('ALTER TABLE "<Task>" ALTER COLUMN "boardId" DROP NOT NULL');
     }
-
 }
