fa01feffc0383e32ae5667bc878df6aeb482e09d
Ivan Shvets <shvets.ivan@gmail.com>
2021-06-26 22:00:36 +0300

    add: jwt refactor

--- src/resources/login/login.repository.ts
+++ src/resources/login/login.repository.ts
@@ -6,9 +6,10 @@ import { User } from '../entities/user.intity';
 const generateToken = async (login: string, password: string) => {
   try {
     const userRepo = getRepository(User);
-    const user = await userRepo.find({ login, password });
-    const payload = { userId: user[0]?.id, login: user[0]?.login };
+    const user = await userRepo.findOne({ login, password });
+    const payload = { userId: user?.id, login: user?.login };
     const token = jwt.sign(payload, String(JWT_SECRET_KEY));
+
     return token;
   } catch (e) {
     throw new Error(e);
